e1e8a92b8fe30d3ea76dd66f846969374e9fa6b6
Bayarmaa <bayarmaa@nashatech.com>
2025-11-30 01:28:29 +0800

    fix24

--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -1,9 +1,7 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
 import slackRoutes from "@api/modules/slack/routes/index";
 
-const routes = new OpenAPIHono()
-	.basePath("/api")
-	.route("/slack", slackRoutes);
+const routes = new OpenAPIHono().route("/api/slack", slackRoutes);
 
 export default routes;
 
